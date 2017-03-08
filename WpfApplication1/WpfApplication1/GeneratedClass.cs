67ebdae1d4eb4afd230a5a8d9d706794e922a8c0
Thomas Tatum <thomasbtatum@followyourshot.com>
2017-03-08 23:27:32 +0100

    add good generate styles.

--- WpfApplication1/WpfApplication1/GeneratedClass.cs
+++ WpfApplication1/WpfApplication1/GeneratedClass.cs
@@ -2,6 +2,8 @@
 
 //Keep this namespace
 using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using DocumentFormat.OpenXml.Office2013.Excel;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using System.Collections.Generic;
@@ -39,6 +41,7 @@ namespace WpfGenerateExcel
                 Sheets sheets = workbook.WorkbookPart.Workbook.GetFirstChild<Sheets>();
                 string relationshipId = workbook.WorkbookPart.GetIdOfPart(sheetPart);
 
+                //AddStyle(workbookPart, relationshipId);
                 WorkbookStylesPart workbookStylesPart = workbook.WorkbookPart.AddNewPart<WorkbookStylesPart>("rIdStyles");
                 GenerateWorkbookStylesPart1Content(workbookStylesPart);
 
@@ -152,19 +155,123 @@ namespace WpfGenerateExcel
             return value;
         }
 
-        //private void AddStyle(WorkbookPart workbookPart, string id)
-        //{
-        //    CellFormat lockFormat = new CellFormat() { ApplyProtection = true, Protection = new Protection() { Locked = true } };
-        //    WorkbookStylesPart sp = workbookPart.GetPartsOfType<WorkbookStylesPart>().FirstOrDefault();
+        private void AddStyle(WorkbookPart workbookPart, string id)
+        {
+            CellFormat lockFormat = new CellFormat() { ApplyProtection = true, Protection = new Protection() { Locked = true } };
+            WorkbookStylesPart sp = workbookPart.GetPartsOfType<WorkbookStylesPart>().FirstOrDefault();
 
-        //    if (sp == null)
-        //        sp = workbookPart.AddNewPart<WorkbookStylesPart>();
-        //    sp.Stylesheet = new Stylesheet();
-        //    sp.Stylesheet.CellFormats = new CellFormats();
-        //    sp.Stylesheet.CellFormats.AppendChild<CellFormat>(lockFormat);
-        //    sp.Stylesheet.CellFormats.Count = UInt32Value.FromUInt32((uint)sp.Stylesheet.CellFormats.ChildElements.Count);
-        //    sp.Stylesheet.Save();
-        //}
+            if (sp == null)
+                sp = workbookPart.AddNewPart<WorkbookStylesPart>();
+            sp.Stylesheet = new Stylesheet();
+            sp.Stylesheet.CellFormats = new CellFormats();
+            sp.Stylesheet.CellFormats.AppendChild<CellFormat>(lockFormat);
+            sp.Stylesheet.CellFormats.Count = UInt32Value.FromUInt32((uint)sp.Stylesheet.CellFormats.ChildElements.Count);
+            sp.Stylesheet.Save();
+        }
+
+        private static void GenerateWorkbookStylesPart1Content(WorkbookStylesPart workbookStylesPart1)
+        {
+            Stylesheet stylesheet1 = new Stylesheet() { MCAttributes = new MarkupCompatibilityAttributes() { Ignorable = "x14ac x16r2" } };
+            stylesheet1.AddNamespaceDeclaration("mc", "http://schemas.openxmlformats.org/markup-compatibility/2006");
+            stylesheet1.AddNamespaceDeclaration("x14ac", "http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac");
+            stylesheet1.AddNamespaceDeclaration("x16r2", "http://schemas.microsoft.com/office/spreadsheetml/2015/02/main");
+
+            Fonts fonts1 = new Fonts() { Count = (UInt32Value)1U, KnownFonts = true };
+
+            Font font1 = new Font();
+            FontSize fontSize1 = new FontSize() { Val = 11D };
+            DocumentFormat.OpenXml.Spreadsheet.Color color1 = new DocumentFormat.OpenXml.Spreadsheet.Color() { Theme = (UInt32Value)1U };
+            FontName fontName1 = new FontName() { Val = "Calibri" };
+            FontFamilyNumbering fontFamilyNumbering1 = new FontFamilyNumbering() { Val = 2 };
+            FontScheme fontScheme1 = new FontScheme() { Val = FontSchemeValues.Minor };
+
+            font1.Append(fontSize1);
+            font1.Append(color1);
+            font1.Append(fontName1);
+            font1.Append(fontFamilyNumbering1);
+            font1.Append(fontScheme1);
+
+            fonts1.Append(font1);
+
+            Fills fills1 = new Fills() { Count = (UInt32Value)2U };
+
+            Fill fill1 = new Fill();
+            PatternFill patternFill1 = new PatternFill() { PatternType = PatternValues.None };
+
+            fill1.Append(patternFill1);
+
+            Fill fill2 = new Fill();
+            PatternFill patternFill2 = new PatternFill() { PatternType = PatternValues.Gray125 };
+
+            fill2.Append(patternFill2);
+
+            fills1.Append(fill1);
+            fills1.Append(fill2);
+
+            Borders borders1 = new Borders() { Count = (UInt32Value)1U };
+
+            Border border1 = new Border();
+            LeftBorder leftBorder1 = new LeftBorder();
+            RightBorder rightBorder1 = new RightBorder();
+            TopBorder topBorder1 = new TopBorder();
+            BottomBorder bottomBorder1 = new BottomBorder();
+            DiagonalBorder diagonalBorder1 = new DiagonalBorder();
+
+            border1.Append(leftBorder1);
+            border1.Append(rightBorder1);
+            border1.Append(topBorder1);
+            border1.Append(bottomBorder1);
+            border1.Append(diagonalBorder1);
+
+            borders1.Append(border1);
+
+            CellStyleFormats cellStyleFormats1 = new CellStyleFormats() { Count = (UInt32Value)1U };
+            CellFormat cellFormat1 = new CellFormat() { NumberFormatId = (UInt32Value)0U, FontId = (UInt32Value)0U, FillId = (UInt32Value)0U, BorderId = (UInt32Value)0U };
+
+            cellStyleFormats1.Append(cellFormat1);
+
+            CellFormats cellFormats1 = new CellFormats() { Count = (UInt32Value)1U };
+            CellFormat cellFormat2 = new CellFormat() { NumberFormatId = (UInt32Value)0U, FontId = (UInt32Value)0U, FillId = (UInt32Value)0U, BorderId = (UInt32Value)0U, FormatId = (UInt32Value)0U };
+
+            cellFormats1.Append(cellFormat2);
+
+            CellStyles cellStyles1 = new CellStyles() { Count = (UInt32Value)1U };
+            CellStyle cellStyle1 = new CellStyle() { Name = "Normal", FormatId = (UInt32Value)0U, BuiltinId = (UInt32Value)0U };
+
+            cellStyles1.Append(cellStyle1);
+            DocumentFormat.OpenXml.Office2010.Excel.DifferentialFormats differentialFormats1 = new DocumentFormat.OpenXml.Office2010.Excel.DifferentialFormats() { Count = (UInt32Value)0U };
+            TableStyles tableStyles1 = new TableStyles() { Count = (UInt32Value)0U, DefaultTableStyle = "TableStyleMedium2", DefaultPivotStyle = "PivotStyleLight16" };
+
+            StylesheetExtensionList stylesheetExtensionList1 = new StylesheetExtensionList();
+
+            StylesheetExtension stylesheetExtension1 = new StylesheetExtension() { Uri = "{EB79DEF2-80B8-43e5-95BD-54CBDDF9020C}" };
+            stylesheetExtension1.AddNamespaceDeclaration("x14", "http://schemas.microsoft.com/office/spreadsheetml/2009/9/main");
+            SlicerStyles slicerStyles1 = new SlicerStyles() { DefaultSlicerStyle = "SlicerStyleLight1" };
+
+            stylesheetExtension1.Append(slicerStyles1);
+
+            StylesheetExtension stylesheetExtension2 = new StylesheetExtension() { Uri = "{9260A510-F301-46a8-8635-F512D64BE5F5}" };
+            stylesheetExtension2.AddNamespaceDeclaration("x15", "http://schemas.microsoft.com/office/spreadsheetml/2010/11/main");
+            TimelineStyles timelineStyles1 = new TimelineStyles() { DefaultTimelineStyle = "TimeSlicerStyleLight1" };
+
+            stylesheetExtension2.Append(timelineStyles1);
+
+            stylesheetExtensionList1.Append(stylesheetExtension1);
+            stylesheetExtensionList1.Append(stylesheetExtension2);
+
+            stylesheet1.Append(fonts1);
+            stylesheet1.Append(fills1);
+            stylesheet1.Append(borders1);
+            stylesheet1.Append(cellStyleFormats1);
+            stylesheet1.Append(cellFormats1);
+            stylesheet1.Append(cellStyles1);
+            stylesheet1.Append(differentialFormats1);
+            stylesheet1.Append(tableStyles1);
+            stylesheet1.Append(stylesheetExtensionList1);
+
+            workbookStylesPart1.Stylesheet = stylesheet1;
+        }
+
 
     }
 }
